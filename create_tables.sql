5145c5a55fc308c8356dabc3e2c0da8b29fc1c2f
Nick-Getka <ngetka@ngetka.localdomain>
2017-03-31 00:07:02 -0400

    8=======D

---
+++ create_tables.sql
@@ -0,0 +1,24 @@
+/*
+This Script creates the 10 Required Tables
+*/
+DROP TABLE customer;
+DROP TABLE customer_preferences;
+DROP TABLE customer_design;
+DROP TABLE customer_artwork;
+DROP TABLE design_template;
+DROP TABLE product;
+DROP TABLE product_category;
+DROP TABLE product_options;
+DROP TABLE customer_product_details;
+DROP TABLE purchase_order;
+
+CREATE TABLE customer();
+CREATE TABLE customer_preferences();
+CREATE TABLE customer_design();
+CREATE TABLE customer_artwork();
+CREATE TABLE design_template();
+CREATE TABLE product();
+CREATE TABLE product_category();
+CREATE TABLE product_options();
+CREATE TABLE customer_product_details();
+CREATE TABLE purchase_order();
