660753ddc86917e6e01240cdaeaa706e5517efff
Nick-Getka <ngetka@ngetka.localdomain>
2017-03-31 03:38:39 -0400

    sdf

--- read_data.sql
+++ read_data.sql
@@ -8,11 +8,9 @@ CREATE TABLE temp_product(
   product_name VARCHAR(100),
   product_description VARCHAR(100),
   price VARCHAR(100),
-  product_option VARCHAR(100),
   option_value VARCHAR(100),
   nothing_useful VARCHAR(100),
-  product_option VARCHAR(100),
-  option_value VARCHAR(100)
+  product_option VARCHAR(100)
   );
 
-\COPY product(product_category, product_name, product_description, price, product_option, option_value, nothing_useful, product_option, option_value) FROM 'final-data/products.dat' WITH CSV HEADER DELIMITER '|'
+\COPY product(product_category, product_name, product_description, price, option_value, nothing_useful, product_option) FROM 'final-data/products.dat' WITH CSV HEADER DELIMITER '|'
