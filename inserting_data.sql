fdd20806fe09330ec7112961463fe103fd1e7be4
Nick-Getka <ngetka@ngetka.localdomain>
2017-03-31 08:48:20 -0400

    sdf

--- inserting_data.sql
+++ inserting_data.sql
@@ -11,15 +11,14 @@ FROM temp_product;
 
 
 /*Inserting Customer Data*/
-CREATE TABLE full_address AS (
-  SELECT email, street, city, state, temp_address.zip
+DROP TABLE IF EXISTS full_address CASCADE;
+CREATE TABLE full_address AS (                                                                       SELECT email, street, city, state, temp_address.zip
   FROM temp_address
-  INNER JOIN temp_zip
-  ON temp_address.zip = temp_zip.zip
-  )
+  LEFT JOIN temp_zip
+  ON temp_address.zip = temp_zip.zip);
 
 INSERT INTO customer(customer_email, customer_password, first_name, last_name, street_address, city, state, zip_code)
-SELECT DISTINCT email_1, user_password, first_name, last_name, street, city, state, zip_code
+SELECT DISTINCT email, user_password, first_name, last_name, street, city, state, zip
 FROM temp_user
-OUTER JOIN full_address
-ON full_address.email = temp_user.email_1
+FULL JOIN full_address
+ON full_address.email = temp_user.email;
