dc46ac1b2015b99ac4380d20943443436c52a7e0
Nick-Getka <ngetka@ngetka.localdomain>
2017-03-31 02:47:51 -0400

    sdf

--- create_tables.sql
+++ create_tables.sql
@@ -5,10 +5,10 @@ DROP TABLE IF EXISTS customer_preferences;
 DROP TABLE IF EXISTS customer_artwork;
 DROP TABLE IF EXISTS customer_design;
 DROP TABLE IF EXISTS design_template;
-DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS product_category;
 DROP TABLE IF EXISTS product_options;
 DROP TABLE IF EXISTS customer_product_details;
+DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS purchase_order;
 DROP TABLE IF EXISTS customer;
 
