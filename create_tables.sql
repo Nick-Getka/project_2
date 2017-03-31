ceab0b4cb9673d845b85797ec1ad6ab044acb45a
Nick-Getka <ngetka@ngetka.localdomain>
2017-03-31 01:15:38 -0400

    sdf

--- create_tables.sql
+++ create_tables.sql
@@ -17,13 +17,13 @@ CREATE TABLE customer(
   customer_email VARCHAR(45) NOT NULL,
   customer_password VARCHAR(20),
   first_name VARCHAR(20),
-  last_name VARCHAR(20)
+  last_name VARCHAR(20),
 
   UNIQUE(customer_id),
   PRIMARY KEY(customer_id)
   );
 CREATE TABLE customer_preferences(
-  
+
   );
 CREATE TABLE customer_design();
 CREATE TABLE customer_artwork();
