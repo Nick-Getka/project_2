a5e6f3cce472d1844ebb1273a8b47aa07447a028
Nick-Getka <ngetka@ngetka.localdomain>
2017-03-31 04:12:56 -0400

    sdf

--- read_data.sql
+++ read_data.sql
@@ -16,13 +16,31 @@ CREATE TABLE temp_product(
   );
 \COPY temp_product(product_category, product_name, product_description, price, product_option_1, option_value_1, nothing_useful, product_option_2, option_value_2) FROM 'final-data/products.dat' WITH CSV HEADER DELIMITER ';'
 
-Drop TABLE IF EXISTS temp_user CASCADE;
+DROP TABLE IF EXISTS temp_user CASCADE;
 CREATE TABLE temp_user(
-  email VARCHAR(100),
+  email_1 VARCHAR(100),
   user_password VARCHAR(100),
-  email VARCHAR(100),
+  email_2 VARCHAR(100),
   first_name VARCHAR(100),
   last_name VARCHAR(100),
   middle_initial VARCHAR(100)
   );
-\COPY temp_user(email, user_password, email, first_name, last_name, middle_initial) FROM 'final-data/user.dat' DELIMITER '|'
+\COPY temp_user(email_1, user_password, email_2, first_name, last_name, middle_initial) FROM 'final-data/user.dat' WITH DELIMITER '|'
+
+DROP TABLE IF EXISTS temp_address CASCADE;
+CREATE TABLE temp_address(
+  email VARCHAR(100),
+  street VARCHAR(100),
+  city VARCHAR(100),
+  zip VARCHAR(100)
+  );
+\COPY temp_addr(email, street, city, zip) FROM 'final-data/address.dat' WITH DELIMITER '|'
+
+DROP TABLE IF EXISTS temp_zip CASCADE;
+CREATE TABLE temp_zip(
+  email VARCHAR(100),
+  street VARCHAR(100),
+  city VARCHAR(100),
+  zip VARCHAR(100)
+  );
+\COPY temp_zip(email, street, city, zip) FROM 'final-data/zip.dat' WITH DELIMITER '|'
